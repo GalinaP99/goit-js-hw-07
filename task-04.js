76edac92fc5738b46d97271c3fdf626a86501062
Galina Pankevych <gln99@ukr.net>
2021-01-08 18:28:29 +0200

    додає 4-те д.з

--- task-04.js
+++ task-04.js
@@ -7,4 +7,22 @@
   <button type="button" data-action="decrement">-1</button>
   <span id="value">0</span>
   <button type="button" data-action="increment">+1</button>
-</div>*/
+</div>
+<body>
+
+Счётчик: <input type="button" value="1" data-counter>
+Ещё счётчик: <input type="button" value="2" data-counter>
+
+<script>
+  document.addEventListener('click', function(event) {
+
+    if (event.target.dataset.counter != undefined) { // если есть атрибут...
+      event.target.value++;
+    }
+
+  });
+</script>
+</body>*/
+
+const counterValue = document.querySelector('#value');
+ //counterValue.addEventListener
